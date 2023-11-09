2f81a96d4945ca8ad1fa4cc4073e8edb209702c8
Holanda03 <carlosabraao@hotmail.com.br>
2023-11-09 14:07:08 -0300

    criacao da pagina de mentores

--- assets/mentor/mentor.component.ts
+++ assets/mentor/mentor.component.ts
@@ -1,11 +1,25 @@
 import { Component } from "@angular/core";
+import { Body } from "@angular/http/src/body";
 
 @Component ({
     selector: 'app-mentor',
     templateUrl: './mentor.component.html',
-
+    styleUrls: ['./mentor.component.css']
 })
 
 export class MentorComponent {
-    
+    mentorArray: any[] = []
+    currentMenorId = "";
+    name: string = "";
+    address: string = "";
+    phone: string = "";
+
+    save() {
+        let bodyData = {
+            "name" : this.name,
+            "address" : this.address,
+            "phone" : this.phone, 
+        };
+        console.log(bodyData);
+    }
 }
